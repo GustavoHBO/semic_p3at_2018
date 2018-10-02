dc5945c1b2d59ef41241a5d00bedb2910b55d2c0
gustavohbo <ghboliveira@hotmail.com>
2018-10-02 19:01:19 -0300

    Nodo for controlle the rotation of the robot

--- src/semic_p3at_rotate.cpp
+++ src/semic_p3at_rotate.cpp
@@ -1,26 +1,70 @@
-#include "ros/ros.h"
-#include "geometry_msgs/Twist.h"
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <nav_msgs/Odometry.h>
+#include <std_msgs/Int16.h>
 #include <sstream>
 
-#define RAD 0.0174533
+#define RAD 57.2958
 
-int main(int argc, char **argv){
-    ros::init(argc, argv, "semic_p3at");
+void rotateCallback(const std_msgs::Int16 angle){
+    int i = 0;
+    
     ros::NodeHandle n;
-    //ros::Publisher chatter_pub = n.advertise<std_msgs::String>("message", 1000);
-    ros::Publisher pub = n.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 1000);
-    ros::Rate loop_rate(10);
+    ros::Publisher pub = n.advertise<geometry_msgs::Twist>("/RosAria/cmd_vel", 1000);
+    ros::Rate loop_rate(1);
     while (ros::ok()){
         geometry_msgs::Twist twist;
-        //std_msgs::String msg;
-        //std::stringstream ss;
-        //ss << " I am the example1_a node ";
-        //msg.data = ss.str();
-        twist.angular.z = 10;
-        //ROS_INFO("%s", msg.data.c_str());
+        ROS_INFO("Rotating");
+        twist.angular.x = 0;
+        twist.angular.y = 0;
+        twist.angular.z = (angle.data/RAD);
         pub.publish(twist);
-        ros::spinOnce();
         loop_rate.sleep();
+        break;
     }
-    return 0;
+}
+
+int main(int argc, char **argv){
+    /**
+     * The ros::init() function needs to see argc and argv so that it can perform
+     * any ROS arguments and name remapping that were provided at the command line.
+     * For programmatic remappings you can use a different version of init() which takes
+     * remappings directly, but for most command-line programs, passing argc and argv is
+     * the easiest way to do it.  The third argument to init() is the name of the node.
+     *
+     * You must call one of the versions of ros::init() before using any other
+     * part of the ROS system.
+     */
+    ros::init(argc, argv, "semic_p3at_rotate");
+
+    /**
+     * NodeHandle is the main access point to communications with the ROS system.
+     * The first NodeHandle constructed will fully initialize this node, and the last
+     * NodeHandle destructed will close down the node.
+     */
+    ros::NodeHandle n;
+
+    /**
+     * The subscribe() call is how you tell ROS that you want to receive messages
+     * on a given topic.  This invokes a call to the ROS
+     * master node, which keeps a registry of who is publishing and who
+     * is subscribing.  Messages are passed to a callback function, here
+     * called chatterCallback.  subscribe() returns a Subscriber object that you
+     * must hold on to until you want to unsubscribe.  When all copies of the Subscriber
+     * object go out of scope, this callback will automatically be unsubscribed from
+     * this topic.
+     *
+     * The second parameter to the subscribe() function is the size of the message
+     * queue.  If messages are arriving faster than they are being processed, this
+     * is the number of messages that will be buffered up before beginning to throw
+     * away the oldest ones.
+     */
+    ros::Subscriber sub = n.subscribe("/semic/p3at/rotate", 1000, rotateCallback);
+
+    /**
+     * ros::spin() will enter a loop, pumping callbacks.  With this version, all
+     * callbacks will be called from within this thread (the main one).  ros::spin()
+     * will exit when Ctrl-C is pressed, or the node is shutdown by the master.
+     */
+    ros::spin();
 }
