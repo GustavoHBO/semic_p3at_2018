77834a824ed6d610854b80de3066edb3047bf1a8
gustavohbo <ghboliveira@hotmail.com>
2018-10-08 18:57:25 -0300

    Using threads for nodes

--- src/semic_p3at_move.cpp
+++ src/semic_p3at_move.cpp
@@ -1,84 +1,81 @@
 #include <ros/ros.h>
+#include <boost/thread.hpp>
 #include <nav_msgs/Odometry.h>
 #include <geometry_msgs/Twist.h>
-#include "std_msgs/String.h"
-#include "std_msgs/UInt64.h"
+#include <std_msgs/Float32.h>
 
-/**
- * This tutorial demonstrates simple receipt of messages over the ROS system.
- */
+double d0 = 0;
+double d1 = 0;
+double distance = 0;
+double t0 = 0;
+double t1 = 0;
+static boost::mutex mutex;
 
-void moveCallback(const std_msgs::UInt64 distance){
-    
-    int i = 0;
-    int speed = 10;
-    
+void odomCallback(nav_msgs::Odometry odom){
+    //mutex.lock();
+    d1 = odom.pose.pose.position.x;
+    ROS_INFO("Odometry: %f", d1);
+    ROS_INFO("Odometry Diference: %f", d1-d0);
+    //mutex.unlock();
+}
+
+void threadOdometry(){
     ros::NodeHandle n;
-    ros::Publisher pub = n.advertise<geometry_msgs::Twist>("/RosAria/cmd_vel", 1000);
-    ros::Rate loop_rate(10);
-    double t0, t1;
-    while (ros::ok()){
-        t0 = ros::Time::now().toSec();
-        double current_distance = 0;
-        geometry_msgs::Twist twist;
-        while(current_distance < distance.data){
-            ROS_INFO("Moving");
-            twist.linear.x = speed;
-            twist.linear.y = 0;
-            twist.linear.z = 0;
-            pub.publish(twist);
-            t1 = ros::Time::now().toSec();
-            current_distance = speed * (t1-t0);
-        }
-        twist.linear.x = 0;
-        pub.publish(twist);
-        break;
+    ros::Subscriber sub1 = n.subscribe("/RosAria/pose", 1, &odomCallback);
+    while(ros::ok()){
+        ros::spinOnce();
     }
 }
 
-int main(int argc, char **argv){
-    /**
-     * The ros::init() function needs to see argc and argv so that it can perform
-     * any ROS arguments and name remapping that were provided at the command line.
-     * For programmatic remappings you can use a different version of init() which takes
-     * remappings directly, but for most command-line programs, passing argc and argv is
-     * the easiest way to do it.  The third argument to init() is the name of the node.
-     *
-     * You must call one of the versions of ros::init() before using any other
-     * part of the ROS system.
-     */
-    ros::init(argc, argv, "semic_p3at_move");
-
-    /**
-     * NodeHandle is the main access point to communications with the ROS system.
-     * The first NodeHandle constructed will fully initialize this node, and the last
-     * NodeHandle destructed will close down the node.
-     */
+void moveCallback(std_msgs::Float32 dist){
+    //mutex.lock();
+    double speed = 0.5;
+    distance = dist.data;
+    ROS_INFO("Distance : %f", distance);
+    ROS_INFO("Distance D1 : %f", d1);
+    d0 = d1;
+    t0 = ros::Time::now().toSec();
+    t1 = t0;
+    //mutex.unlock();
     ros::NodeHandle n;
-
-    /**
-     * The subscribe() call is how you tell ROS that you want to receive messages
-     * on a given topic.  This invokes a call to the ROS
-     * master node, which keeps a registry of who is publishing and who
-     * is subscribing.  Messages are passed to a callback function, here
-     * called chatterCallback.  subscribe() returns a Subscriber object that you
-     * must hold on to until you want to unsubscribe.  When all copies of the Subscriber
-     * object go out of scope, this callback will automatically be unsubscribed from
-     * this topic.
-     *
-     * The second parameter to the subscribe() function is the size of the message
-     * queue.  If messages are arriving faster than they are being processed, this
-     * is the number of messages that will be buffered up before beginning to throw
-     * away the oldest ones.
-     */
-    ros::Subscriber sub = n.subscribe("/semic/p3at/move", 1000, moveCallback);
-
-    /**
-     * ros::spin() will enter a loop, pumping callbacks.  With this version, all
-     * callbacks will be called from within this thread (the main one).  ros::spin()
-     * will exit when Ctrl-C is pressed, or the node is shutdown by the master.
-     */
-    ros::spin();
-
-    return 0;
+    ros::Publisher pub = n.advertise<geometry_msgs::Twist>("/RosAria/cmd_vel", 1);
+    while(ros::ok()){
+        geometry_msgs::Twist twist;
+        //mutex.lock();
+        
+        if(d1 - d0 < (distance - (speed*(t1-t0)))){
+            twist.linear.x = speed;
+            pub.publish(twist);
+            t1 = ros::Time::now().toSec();
+        } else {
+            twist.linear.x = 0;
+            ROS_INFO("Parando!");
+            pub.publish(twist);
+            //mutex.unlock();
+            break;
+        }
+        //mutex.unlock();
+    }
 }
+
+void threadMove(){
+    ros::NodeHandle n;
+    ros::Subscriber sub2 = n.subscribe("/semic/p3at_move", 1, &moveCallback);
+    while(ros::ok()){
+        ros::spinOnce();
+    }
+}
+
+int main(int argc, char** argv){
+    ros::init(argc, argv, "test");
+    ros::NodeHandle node;
+    boost::thread thread_a(threadOdometry);
+    boost::thread thread_b(threadMove);
+     
+    ros::MultiThreadedSpinner spinner(2); // Use 4 threads
+    spinner.spin(); // spin() will not return until the node has been shutdown
+    //ros::Subscriber sub2 = node.subscribe("/semic/p3at_move", 10, &topicBCallback);
+    //ros::Timer timer = node.createTimer(ros::Duration(0.1),timerCallback);
+    //ros::spin();
+    return 0;
+};
